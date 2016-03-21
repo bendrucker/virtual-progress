b31cfe46eb3d4bfb4257bd520f7953ea93743d75
Ben Drucker <bvdrucker@gmail.com>
2016-03-21 16:42:38 -0700

    Add tests

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
 'use strict'
 
 var test = require('tape')
-var virtualProgress = require('./')
+var rent = require('rent')
+var pipe = require('value-pipe')
+var Progress = require('./')
 
 test(function (t) {
+  var state = Progress()
+  var render = pipe(rent(Progress.render, state), bar)
 
+  t.equal(render().properties.style.transform, 'translate3d(-100%, 0, 0)')
+
+  state.value.set(0.5)
+  t.equal(render().properties.style.transform, 'translate3d(-50%, 0, 0)')
+
+  t.end()
 })
+
+function bar (vtree) {
+  return vtree.children[0].children[0]
+}
